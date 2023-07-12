4296b7d70b78a1d2b8a892e35202ee0f0b215140
abaez8031 <abaez8031@gmail.com>
2023-07-12 19:24:15 -0400

    some things

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ print(age)
 
 name = input("What is your name? ")
 print("Hello " + name)
+
+birth_year = input("Enter your birth year: ")
+age = 2023 - int(birth_year)
+print(age)
+int()
+float()
+bool()
+str()
+
+input_1 = float(input("First: "))
+input_2 = float(input("Second: "))
+print(input_1 + input_2)
