4a90bf71b5d60039616efbf927c174d607fd635c
abaez8031 <abaez8031@gmail.com>
2023-07-13 16:44:49 -0400

    logical operators

--- app.py
+++ app.py
@@ -20,10 +20,21 @@
 # sum = input_1 + input_2
 # print("Sum: " + str(sum))
 
-course = "Python for Beginners"
-print(course.upper())
-print(course.find("y"))
-print(course.find("Y"))
-print(course.replace("for", "4"))
-print("Python" in course)
-print(course)
+# course = "Python for Beginners"
+# print(course.upper())
+# print(course.find("y"))
+# print(course.find("Y"))
+# print(course.replace("for", "4"))
+# print("Python" in course)
+# print(course)
+
+# print(10 / 3)  # Returns float
+# print(10 // 3)  # Returns integer
+# print(10 ** 3)  # Exponent
+
+price = 5
+print(price > 10 or price < 30)
+print(price > 10 and price < 30)
+print (price > 10)
+print(not price > 10)
+
