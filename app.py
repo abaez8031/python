18712864fd4f731176a8c71d9655eea3c7c13d89
abaez8031 <abaez8031@gmail.com>
2023-07-13 02:34:08 -0400

    string methods

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-age = 20
-price = 19.95
-first_name = "Mosh"
-is_online = True
-print(age)
+# age = 20
+# price = 19.95
+# first_name = "Mosh"
+# is_online = True
+# print(age)
+#
+# name = input("What is your name? ")
+# print("Hello " + name)
+#
+# birth_year = input("Enter your birth year: ")
+# age = 2023 - int(birth_year)
+# print(age)
+# int()
+# float()
+# bool()
+# str()
+#
+# input_1 = float(input("First: "))
+# input_2 = float(input("Second: "))
+# sum = input_1 + input_2
+# print("Sum: " + str(sum))
 
-name = input("What is your name? ")
-print("Hello " + name)
-
-birth_year = input("Enter your birth year: ")
-age = 2023 - int(birth_year)
-print(age)
-int()
-float()
-bool()
-str()
-
-input_1 = float(input("First: "))
-input_2 = float(input("Second: "))
-print(input_1 + input_2)
+course = "Python for Beginners"
+print(course.upper())
