7206cacce07c96f6ae7dfff402aec3014d24648f
abaez8031 <abaez8031@gmail.com>
2023-07-15 00:40:40 -0400

    classes

--- app.py
+++ app.py
@@ -139,11 +139,14 @@
 # print("Finish")
 
 class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
     def move(self):
         print("Move")
     def draw(self):
         print("Draw")
 
 
-point = Point()
-# print(point.x)
+point = Point(10,20)
+print(point.x)
